bb14e3b6d83fa72e9a6c299fb835194bb7bc96cc
Daniel Wallen <daniel.wallen1987@gmail.com>
2020-02-11 16:56:55 -0500

    Added code for longest chain function.

--- src/collisions.py
+++ src/collisions.py
@@ -26,4 +26,25 @@ def how_many_loops_before_collision(loops, array_length):
         # ...print a statement specifying the array length and the number of tries that occured before we had a collisiong
         print("We had {} buckets and {} hashes before a collision".format(array_length, tries))
 
-how_many_loops_before_collision(10, 100)
+# how_many_loops_before_collision(10, 100)
+
+def longest_linked_list_chain(keys, buckets, loops): 
+    for i in range(loops):
+        key_counts = {}
+
+        for i in range(buckets):
+            key_counts[i] = 0
+
+        for i in range(keys):
+            random_key = str(random.random())
+            hash_index = hash(random_key) % buckets
+            key_counts[hash_index] += 1
+
+        largest_n = 0
+        for key in key_counts:
+            if key_counts[key] > largest_n:
+                largest_n = key_counts[key]
+
+        print(f"Longest Linked List Chain for {keys} keys in {buckets} buckets (Load Factor: {keys/buckets:2f}: {largest_n})")
+
+longest_linked_list_chain(16, 16, 10)
